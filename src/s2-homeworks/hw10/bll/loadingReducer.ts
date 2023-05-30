a8895ff4903f5344c1e85f900e9be24205908444
ValeriaBog <valeriabog@yandex.ru>
2023-05-30 12:57:35 +0300

    done task H10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,10 +2,19 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+
+type ActionTypes = LoadingActionType
+
+export type InitialStateType = {
+    isLoading: boolean
+}
+
+export const loadingReducer = (state:InitialStateType  = initState, action: ActionTypes): InitialStateType => { // fix any
+
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case "CHANGE_LOADING":
+            return  {...state, isLoading: action.isLoading}
         default:
             return state
     }
